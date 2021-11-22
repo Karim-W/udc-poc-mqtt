e32e3382e9ac89f44e11e5b97d926492edede39a
Karim-W <Karim.wael@gmail.com>
2021-11-22 04:03:37 +0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Enduser from './Pages/enduser/Enduser';
+import Admin from './Pages/admin/admin';
+import Home from './Pages/Home/Home';
+import { Connector } from 'mqtt-react-hooks';
+import * as mqtt from 'mqtt';
+import { MqttClientContext } from './Contexts/mqttClientContext';
 function App() {
+  const [client, setClient] = useState<mqtt.Client>();
+  const [connectionStatus, setConnectStatus] = useState('');
+  const mqttClientValue = useMemo(
+    () => ({ client, setClient }),
+    [client, setClient]
+  );
+
+  useEffect(() => {
+    console.log('client');
+    var client = mqtt.connect("ws://20.124.99.194:9001")
+    client.on("connect", function () {
+      console.log("Connected")
+      setClient(client)
+      client.publish("topic", "hello from admin")
+    })
+  }, []
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MqttClientContext.Provider value={mqttClientValue}>
+      <Router>
+        <Routes>
+          <Route path="/enduser" element={<Enduser />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </MqttClientContext.Provider>
+
   );
 }
 
 export default App;
+// const mqttConnect = (host: any, mqttOption: any) => {
+  //   setConnectStatus('Connecting');
+  //   const client = mqtt.connect(host, mqttOption);
+  // };
+  // useEffect(() => {
+  //   if (client != null) {
+  //     mqttConnect("")
+  //   }
+  // }, [client]);
+  // useEffect(() => {
+  //   if (client) {
+  //     console.log(client)
+  //     client.on('connect', () => {
+  //       setConnectStatus('Connected');
+  //     });
+  //     client.on('error', (err) => {
+  //       console.error('Connection error: ', err);
+  //       client.end();
+  //     });
+  //     client.on('reconnect', () => {
+  //       setConnectStatus('Reconnecting');
+  //     });
+  //     client.on('message', (topic, message) => {
+  //       const payload = { topic, message: message.toString() };
+  //       setPayload(payload);
+  //     });
+  //   }
+  // }, [client]);
